0acbbaa7913f2e10dc4751b66b829dffc614a15a
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-11-13 13:37:49 +0200

    2nd run, tried to fix a bit of negative integer stuff

--- Algebra.java
+++ Algebra.java
@@ -6,7 +6,7 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
+	    System.out.println(plus(-222,-33));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
@@ -25,22 +25,42 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
+		if (x2 >0) {
 		for (int i=0; i<x2; i++)
-		{
+			{
 			x1++;
+			}
+		}
+		else if (x2 < 0) 
+		{
+			for (int i=0; i>x2; i--) {
+		
+			x1--;
+			}
+
 		}
 		return x1;
+		
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		for (int i=0; i<x2; i++)
+		if (x2>0)
 		{
+		for (int i=0; i<x2; i++)
+			{
 			x1--;
+			}
 		}
+		else {
+			for (int i=0; i>x2; i--)
+			{
+			x1++;
+			}
+		}
+		
 		return x1;
-	}
-
+}
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int temp = x1;
