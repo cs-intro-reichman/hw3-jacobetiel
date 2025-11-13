abcb502ea8a6e80773a9dd8e01466dfe192620dd
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-11-13 12:35:56 +0200

    first testing

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,76 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i=0; i<x2; i++)
+		{
+			x1++;
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i=0; i<x2; i++)
+		{
+			x1--;
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int temp = x1;
+		for(int i=0; i<x2-1;i++)
+		{
+		x1 = plus(x1,temp);
+		}
+	return x1;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int temp = x; 
+		for (int i=0; i<n-1; i++)
+		{
+			x = times(x,temp);
+		}
+		
+		return x;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
+		if (x1==x2)
+			return 1;
+		
+		for (int i=0; i<(x1/2);i++)
+		{
+			if (times(x2,i)==x1)
+				return i;
+			if (times(x2,i)>x1)
+				return i-1;
+		}
+
+
 		return 0;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int temp = div(x1,x2);
+		return minus(x1,(times(temp,x2)));
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
+		
+		for (int i=1; i<= x/2;i++)
+		{
+			if (pow(i, 2)==x)
+				return i;
+			if (pow(i,2)>x)
+				return i-1;
+		}
 		return 0;
 	}	  	  
 }
